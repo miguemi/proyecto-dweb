b1e468628360823f0c423932e1c25ada570bc8d6
miguemi <manuelmiguel80@hotmail.com>
2023-09-05 15:44:15 -0600

    tutorial models

--- src/core/erp/models.py
+++ src/core/erp/models.py
@@ -1,8 +1,36 @@
 from django.db import models
 from datetime import datetime
 
+
+class Type(models.Model):
+    name = models.TextField(verbose_name='Nombre')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Tipo'
+        verbose_name_plural = 'Tipos'
+        db_table = 'empleado_tipo'
+        ordering = ['id']
+
+class Category(models.Model):
+    
+    name = models.TextField(verbose_name='Nombre')
+
+    def __str__(self):
+        return self.name 
+
+    class Meta:
+        verbose_name = 'Categoria'
+        verbose_name_plural = 'Categorias'
+        ordering = ['id']
+
+            
 # Create your models here.
 class Employee(models.Model):
+    categ = models.ManyToManyField(Category)
+    type = models.ForeignKey(Type, on_delete=models.CASCADE)
     names = models.TextField(verbose_name='Nombres')
     dni = models.TextField(unique=True, verbose_name='Dni')
     date_joined = models.DateField(default=datetime.now) 
