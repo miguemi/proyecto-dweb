eeb2bd186dcda2493499035ba8509c39fc222b4e
miguemi <manuelmiguel80@hotmail.com>
2023-09-06 07:28:30 -0600

    tests del ORM de django

---
+++ activar_entorno.ps1
@@ -0,0 +1,3 @@
+& ".\env\Scripts\Activate.ps1"
+Set-Location ./src/ 
+Write-Host "Entorno de python activado"
--- src/core/erp/tests.py
+++ src/core/erp/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+from app.wsgi import *
+from core.erp.models import Type
 
-# Create your tests here.
+application = get_wsgi_application()
+
+# query = Type.objects.all()
+# print(query)
+
+# insercion
+# t = Type()
+# t.name = 'Presidente'
+# t.save()
+
+#edicion
+# t = Type.objects.get(pk=1)
+# t.name = "prueba"
+# t.save()
+
+# #eliminacion
+# t = Type.objects.get(pk=1)
+# t.delete()
