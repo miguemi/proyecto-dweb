1623e0fa76ce420398cfd096ee032fb566e5b1c7
miguemi <manuelmiguel80@hotmail.com>
2023-09-13 12:35:15 -0600

    Implementacion de UpdateView

--- src/core/base/urls.py
+++ src/core/base/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from core.base.views.category.views import CategoryListView, CategoryCreateView
+from core.base.views.category.views import CategoryListView, CategoryCreateView, CategoryUpdateView
 
 
 app_name = 'category'
 
 urlpatterns = [
     path('list/', CategoryListView.as_view(), name='cat_list'),
-    path('add/', CategoryCreateView.as_view(), name="cat_create")
+    path('add/', CategoryCreateView.as_view(), name="cat_create"),
+    path('edit/<int:pk>', CategoryUpdateView.as_view(), name="cat_edit")
 ]
--- src/core/base/views/category/views.py
+++ src/core/base/views/category/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView
 
 from core.base.forms import CategoryForm
 from core.base.models import Category
@@ -68,4 +68,36 @@ class CategoryCreateView(CreateView):
         context['entity'] = 'Categorias'
         context['list_url'] = reverse_lazy('category:cat_list')
         context['action'] = 'add'
+        return context
+
+
+class CategoryUpdateView(UpdateView):
+    model = Category
+    form_class = CategoryForm
+    template_name = 'category/create.html'
+    success_url = reverse_lazy('category:cat_list')
+
+    def dispatch(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        return super().dispatch(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        data = {}
+        try:
+            action = request.POST['action']
+            if action == 'edit':
+                form = self.get_form()
+                data = form.save()
+            else:
+                data['error'] = 'No ha ingresado a ninguna opción'
+        except Exception as e:
+            data['error'] = str(e)
+        return JsonResponse(data)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Edición una Categoria'
+        context['entity'] = 'Categorias'
+        context['list_url'] = reverse_lazy('category:cat_list')
+        context['action'] = 'edit'
         return context
