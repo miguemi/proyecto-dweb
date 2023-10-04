8f27661ed906a2554e2e7e889c80180e1e71137c
miguemi <manuelmiguel80@hotmail.com>
2023-10-03 20:27:00 -0600

    Implementacion de grupos en apartado de usuarios

--- src/core/user/forms.py
+++ src/core/user/forms.py
@@ -10,7 +10,7 @@ class UserForm(ModelForm):
 
     class Meta:
         model = User
-        fields = 'first_name', 'last_name', 'email', 'username', 'password', 'image'
+        fields = 'first_name', 'last_name', 'email', 'username', 'password', 'image', 'groups'
         widgets = {
             'first_name': TextInput(
                 attrs={
@@ -32,21 +32,36 @@ class UserForm(ModelForm):
                     'placeholder': 'Ingrese su username',
                 }
             ),
-            'password': PasswordInput(
-                render_value=True,
-                attrs={
-                    'placeholder': 'Ingrese su password',
-                }
-            ),
+            'password': PasswordInput(render_value=True,
+                                      attrs={
+                                          'placeholder': 'Ingrese su password',
+                                      }
+                                      ),
+            'groups': SelectMultiple(attrs={
+                'class': 'form-control select2',
+                'style': 'width: 100%',
+                'multiple': 'multiple'
+            })
         }
-        exclude = ['groups', 'user_permissions', 'last_login', 'date_joined', 'is_superuser', 'is_active', 'is_staff']
+        exclude = ['user_permissions', 'last_login', 'date_joined', 'is_superuser', 'is_active', 'is_staff']
 
     def save(self, commit=True):
         data = {}
         form = super()
         try:
             if form.is_valid():
-                form.save()
+                pwd = self.cleaned_data['password']
+                u = form.save(commit=False)
+                if u.pk is None:
+                    u.set_password(pwd)
+                else:
+                    user = User.objects.get(pk=u.pk)
+                    if user.password != pwd:
+                        u.set_password(pwd)
+                u.save()
+                u.groups.clear()
+                for g in self.cleaned_data['groups']:
+                    u.groups.add(g)
             else:
                 data['error'] = form.errors
         except Exception as e:
