af1076ded26db127cac0f22e578abc9aad1cd908
miguemi <manuelmiguel80@hotmail.com>
2023-10-06 11:24:00 -0600

    Maquetacion de reseteo de contraseña completado

--- src/core/login/urls.py
+++ src/core/login/urls.py
@@ -6,5 +6,6 @@ app_name = 'security'
 
 urlpatterns = [
     path('', LoginFormView.as_view(), name='login'),
-    path('logout/', LogoutRedirectView.as_view(), name='logout')
+    path('logout/', LogoutRedirectView.as_view(), name='logout'),
+    path('reset_password/', ResetPasswordView.as_view(), name='reset_password')
 ]
--- src/core/login/views.py
+++ src/core/login/views.py
@@ -1,17 +1,30 @@
-from django.contrib.auth import logout
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.views import LoginView
+from django.http import HttpResponseRedirect, JsonResponse
 from django.shortcuts import redirect
-from django.views.generic import RedirectView
+from django.template.loader import render_to_string
+from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+from django.views.generic import FormView, RedirectView
 
 import app.settings as setting
+from app import settings
+from core.login.forms import ResetPasswordForm
+from core.user.models import User
 
 
 class LoginFormView(LoginView):
-    template_name = 'login.html'
+    template_name = 'login/login.html'
 
     def dispatch(self, request, *args, **kwargs):
         if request.user.is_authenticated:
-            return redirect(setting.LOGIN_REDIRECT_URL)
+            return redirect(settings.LOGIN_REDIRECT_URL)
         return super().dispatch(request, *args, **kwargs)
 
     def get_context_data(self, **kwargs):
@@ -28,3 +41,53 @@ class LogoutRedirectView(RedirectView):
         return super().dispatch(request, *args, **kwargs)
 
 
+class ResetPasswordView(FormView):
+    form_class = ResetPasswordForm
+    template_name = 'login/resetpwd.html'
+    success_url = reverse_lazy(settings.LOGIN_REDIRECT_URL)
+
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
+    def send_email_reset_pwd(self, user):
+        data = {}
+        try:
+            mail_server = smtplib.SMTP(settings.EMAIL_HOST, settings.EMAIL_PORT)
+            mail_server.starttls()
+            mail_server.login(settings.EMAIL_HOST_USER, settings.EMAIL_HOST_PASSWORD)
+
+            email_to = user.email
+            message = MIMEMultipart()
+            message['From'] = settings.EMAIL_HOST_USER
+            message['To'] = email_to
+            message['Subject'] = 'Reseteo de contraseña'
+
+            content = render_to_string('login/send_email.html', {
+                'user': user,
+                'link_resetpwd': '',
+                'link_home': ''
+            })
+            message.attach(MIMEText(content, 'html'))
+            mail_server.sendmail(settings.EMAIL_HOST_USER, email_to, message.as_string())
+        except Exception as e:
+            data['error'] = str(e)
+        return data
+
+    def post(self, request, *args, **kwargs):
+        data = {}
+        try:
+            form = ResetPasswordForm(request.POST)  # self.get_form()
+            if form.is_valid():
+                user = form.get_user()
+                data = self.send_email_reset_pwd(user)
+            else:
+                data['error'] = form.errors
+        except Exception as e:
+            data['error'] = str(e)
+        return JsonResponse(data, safe=False)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Reseteo de Contraseña'
+        return context
